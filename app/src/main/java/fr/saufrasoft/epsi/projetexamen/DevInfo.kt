986a7e244cdc658c3f57176da3d59ffdf84bcd06
François Saura <franois.saura@gmail.com>
2021-12-09 22:19:58 +0100

    Design de la page d'information du développeur 1 (moi) + Backend (lien)

--- app/src/main/java/fr/saufrasoft/epsi/projetexamen/DevInfo.kt
+++ app/src/main/java/fr/saufrasoft/epsi/projetexamen/DevInfo.kt
@@ -2,6 +2,14 @@ package fr.saufrasoft.epsi.projetexamen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+import android.content.ActivityNotFoundException
+
+import android.content.Intent
+import android.net.Uri
+
 
 class DevInfo : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +19,21 @@ class DevInfo : BaseActivity() {
         showBtnBack()
         setHeaderTitle(getString(R.string.titleBar_DevInfo))
 
+        val linkTextViewEPSI = findViewById<TextView>(R.id.linkTextViewEPSI)
+        linkTextViewEPSI.setOnClickListener {
+
+            try {
+                val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.linkEPSI).replace("<u>", "").replace("</u>", "")))
+                startActivity(webIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    this, getString(R.string.errorLinkViewEPSI), Toast.LENGTH_LONG
+                ).show()
+                e.printStackTrace()
+            }
+
+        }
     }
+
 
 }
