6ac4ef46e640b64269d3ac4697fd42d565ee8b82
François Saura <franois.saura@gmail.com>
2022-01-02 23:59:51 +0100

    Ajout de commentaire

--- app/src/main/java/fr/saufrasoft/epsi/projetexamen/CategoryAdapter.kt
+++ app/src/main/java/fr/saufrasoft/epsi/projetexamen/CategoryAdapter.kt
@@ -10,9 +10,12 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
 
+//On spécifie une liste de Category en argument d'instanciation (dans la méthode d'appel) de la classe:
 class CategoryAdapter (private val Categories: ArrayList<Category>): RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
     class ViewHolder (view: View) : RecyclerView.ViewHolder(view){
+        //On récupère les contrôles dans des variables.
+        // (!) Toutes la variables ici seront disponibles en appellant le ViewHolder:
         val tbCategoryTitle = view.findViewById<TextView>(R.id.textViewCategoryTitle)
         val layoutItemRow = view.findViewById<LinearLayout>(R.id.categoryItemRowLayout)
     }
@@ -23,10 +26,15 @@ class CategoryAdapter (private val Categories: ArrayList<Category>): RecyclerVie
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val categorieItem = Categories.get(position)
-        holder.tbCategoryTitle.text = categorieItem.title
+        //Cette fonction est appelé à chaque élément dans notre liste de Category "Categories"
+
+        val categorieItem = Categories.get(position) //On récupère l'élément en cours de traitement dans la liste grâce à son index pour pouvoir lire ces propriétées
+        holder.tbCategoryTitle.text = categorieItem.title //Comme le titre qu'on affichera sur la vue.
 
         holder.layoutItemRow.setOnClickListener(View.OnClickListener {
+            //M'a donner beaucoup de fil a retordre pour pouvoir instancier une Intent et la démarrer depuis ICI. Malgré mes recherches sur Internet je n'ai pas réelement trouver de solution à mon problème mais j'ai finit pas tenter ça et ça fonctionne donc je ne touche plus !:
+            //(Vous nous avez pas vraiment expliquer ce que c'était "Application" lors de l'instanciation d'une Intent comme d'habitude avec Intent(Application, ProductList::class.java)
+
             //val newProductIntent = Intent(holder.layoutItemRow.rootView.context, ProductList::class.java)
             val newProductIntent = Intent(holder.layoutItemRow.context, ProductList::class.java)
             newProductIntent.putExtra("category_title", categorieItem.title)
